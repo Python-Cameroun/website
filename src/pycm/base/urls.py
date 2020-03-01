24b2c54c3104dd46fbab830a0f2dbf08de1c6951
Matrix Jef <jefcolbi@gmail.com>
2020-03-01 20:19:07 +0100

    before including work done

--- src/pycm/base/urls.py
+++ src/pycm/base/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', HomeView.as_view()),
-    path('<str>/', HomeView.as_view()),
     path('signup/', SignupView.as_view()),
+    path('signin/', SigninView.as_view()),
+    path('events/', EventsView.as_view()),
+    path('event/', EventsView.as_view()),
 ]
--- src/pycm/base/views.py
+++ src/pycm/base/views.py
@@ -6,11 +6,36 @@ class HomeView(View):
     """This is the view for Home page"""
     
     def get(self, request, *args, **kwargs):
-        return render(self.request, "base/index.html")
+        return render(self.request, "base/home.html")
 
 
 class SignupView(View):
     """This is the view for signup"""
     
     def get(self, request, *args, **kwargs):
+        print("serving signup")
         return render(self.request, "base/signup.html")
+    
+    
+class SigninView(View):
+    """This is the view for signup"""
+    
+    def get(self, request, *args, **kwargs):
+        print("serving signup")
+        return render(self.request, "base/signin.html")
+
+    
+class EventsView(View):
+    """This is the view for signup"""
+    
+    def get(self, request, *args, **kwargs):
+        print("serving signup")
+        return render(self.request, "base/events.html")
+
+
+class EventsView(View):
+    """This is the view for signup"""
+    
+    def get(self, request, *args, **kwargs):
+        print("serving signup")
+        return render(self.request, "base/event.html")
