3db4f853d0dea16d0c7c10471dd35a747729a90d
Matt Jeff <jefcolbi@gmail.com>
2020-05-09 02:53:08 +0100

    added support for bjoern

---
+++ src/pycm/fast.py
@@ -0,0 +1,11 @@
+import bjoern
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pycm.settings')
+
+application = get_wsgi_application()
+
+bjoern.run(application, '127.0.0.1', 8000)
